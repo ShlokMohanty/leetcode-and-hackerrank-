7da458fb8969ed73998755270fc520d7a0fddb7c
Shlok Mohanty <59838234+ShlokMohanty@users.noreply.github.com>
2022-08-29 02:17:15 +0530

    commented out code for showing dry run

--- Recursion/Fibonacci.java
+++ Recursion/Fibonacci.java
@@ -12,3 +12,8 @@ static int fibo(int n) {
 }
 }
 
+// n=0 : fib(0) : 1 return 0
+// n=1 : fib(1) : 1 return 1
+// n=2 : fib(2) : 2 = 2 : return fib(n-1) + fib(n-2) : fib(1) + fib(0) : 1 + 1 : 2 
+//n=3 : fib(3): 3>2 : return fib(n-1)+fib(n-2): fib(2)+ fib(1) : 2 + 1 : 3
+// n=4 : fib(4): 4>2 : return fib(n-1)+fib(n-2) : fib(3) + fib(2) : 3 + 2 : 5
